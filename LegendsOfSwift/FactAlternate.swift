d88adc362af63d834dfe3ba94833429ad33b8b7a
Jennifer Joseph <josephjz@bc.edu>
2020-12-05 19:04:08 -0500

    Legends of Swift Complete.

--- LegendsOfSwift/FactAlternate.swift
+++ LegendsOfSwift/FactAlternate.swift
@@ -5,4 +5,46 @@
 //  Created by Jennifer Joseph on 12/5/20.
 //
 
+// more efficient than creating Result struct in Fact.swift
+
 import Foundation
+
+class FactAlternate : Codable {     // now the class is codable -- can use this class to directly decode JSON
+                                    // now it needs the value property (exactly how it is in JSON page)
+    
+    var value : String = ""
+    var url = "https://api.chucknorris.io/jokes/random?category=dev"
+    
+    
+    func getData(completed: @escaping ()->()) {
+        let urlString = url // API url
+        print("accessing this given url: \(urlString)")
+        
+        // create URL
+        guard let url = URL(string: urlString) else {
+            print("ERROR: could not create a URL from this given url: \(urlString).")
+            completed()
+            return
+        }
+        
+        // create session
+        let session = URLSession.shared
+        
+        // get data with .dataTask method
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            // deal with the data
+            do {
+                let fact = try JSONDecoder().decode(FactAlternate.self, from: data!)     // our Result class does not exist anymore, so we have to replace it w class that does -- FactAlterate
+                // we cant decode directly into the class we have our data in, so we have to create a local constant (fact) to hold JSON decoded value
+                self.value = fact.value
+            } catch {
+                print("JSON ERROR: \(error.localizedDescription)")
+            }
+            completed()
+        }
+        task.resume()   // DON'T FORGET THIS LINE
+    }
+}
--- LegendsOfSwift/ViewController.swift
+++ LegendsOfSwift/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var genderSegmentedControl: UISegmentedControl!
     @IBOutlet weak var outputLabel: UILabel!
     
-    var fact = Fact()
+    //var fact = Fact()
+    var factAlternate = FactAlternate()
     
     override func viewDidLoad() {
         
@@ -26,11 +27,50 @@ class ViewController: UIViewController {
         lastNameField.text = "Norris"
         
     }
+    
+    
+    func replaceName(quote: String) -> String {
+        var new = quote.replacingOccurrences(of: "chuck", with: firstNameField.text!)
+        new = new.replacingOccurrences(of: "Chuck", with: firstNameField.text!)
+        new = new.replacingOccurrences(of: "norris", with: lastNameField.text!)
+        new = new.replacingOccurrences(of: "Norris", with: lastNameField.text!)
+        new = new.replacingOccurrences(of: " &quot ", with: " \" ")
+        return new
+    }
+    
+    func updateGender(quote: String) -> String {
+        var genderQuote = quote
+        if genderSegmentedControl.selectedSegmentIndex == 1 {
+            genderQuote = genderQuote.replacingOccurrences(of: " he ", with: " she ")
+            genderQuote = genderQuote.replacingOccurrences(of: " (he ", with: " (she ")
+            genderQuote = genderQuote.replacingOccurrences(of: " He ", with: " She ")
+            genderQuote = genderQuote.replacingOccurrences(of: "  him", with: " her")
+            genderQuote = genderQuote.replacingOccurrences(of: "  Him ", with: " Her")
+            genderQuote = genderQuote.replacingOccurrences(of: " his ", with: " hers")
+            genderQuote = genderQuote.replacingOccurrences(of: " His ", with: " Hers")
+            genderQuote = genderQuote.replacingOccurrences(of: " beard ", with: " hair ")
+        }
+        return genderQuote
+    }
+    
 
     @IBAction func getFactPressed(_ sender: UIButton) {
-        fact.getData {
+        // using Fact.swift:
+//        fact.getData {
+//            DispatchQueue.main.async {
+//                 self.outputLabel.text = self.fact.quote
+//            }
+//        }
+        
+        // using FactAlternate.swift:
+        factAlternate.getData {
             DispatchQueue.main.async {
-                self.outputLabel.text = self.fact.quote
+                // using Fact.swift:
+                // self.outputLabel.text = self.fact.quote
+                // using FactAlternate.swift:
+                
+                self.outputLabel.text = self.replaceName(quote: self.updateGender(quote: self.factAlternate.value))
+                //self.outputLabel.text = self.factAlternate.value
             }
         }
     }
